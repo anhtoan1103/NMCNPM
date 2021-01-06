ae10fb3e7a21510a6e985e1d9087b863cab2ced7
QUANG NGUYỄN VINH <18120229@student.hcmus.edu.vn>
2021-01-06 21:16:11 +0700

    admin test

---
+++ Đồ Án/src/public/app/controllers/AdminController.js
@@ -0,0 +1,38 @@
+const Admin = require('../models/admin');
+const User = require('../models/user');
+const {multipleMongooseToObject} = require('../../app/util/mongoose')
+const validator = require('validator');
+
+class AdminController {
+    index(req, res) {
+        res.render('admin', {message: ''});
+    }
+
+    show(req, res) {
+        res.send('admin detail');
+    }
+
+    async admin(req, res) {
+        if(!req.body.email || !validator.isEmail(req.body.email))
+            return res.render('admin', {message: 'Please enter a valid email address.'});
+        if(!req.body.password || !req.body.password.length === 0)
+            return res.render('admin', {message: 'Please enter a valid password.'});
+
+        let admin = await Admin.find({
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        let user = await User.find({});
+        if(admin.length === 1)
+            return res.render('admin_page', {
+                admin: admin,
+                users: multipleMongooseToObject(users)
+            });
+        else {
+            return res.render('admin', {message: 'Không thể tìm thấy user nào dưới thông tin này.'});
+        }
+    }
+}
+
+module.exports = new AdminController
